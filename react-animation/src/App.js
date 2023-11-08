fd6138c9a1143d17e99443d1dde5ad077419cf1a
highnobility <high_nobility@yahoo.com>
2023-11-08 16:55:08 -0500

    basic moving square

--- react-animation/src/App.js
+++ react-animation/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 
 function App() {
+  const [isMoved, setIsMoved] = useState(false);
+
+  const moveAnimation = useSpring({
+    transform: isMoved ? 'translateX(100px)' : 'translateX(0)',
+  });
+
+  const handleButtonClick = () => {
+    setIsMoved(!isMoved);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Spring Animation</h1>
+      <animated.div
+        style={{
+          width: '100px',
+          height: '100px',
+          background: 'lightblue',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          ...moveAnimation,
+        }}
+      >
+        <button onClick={handleButtonClick}>Move Me</button>
+      </animated.div>
     </div>
   );
 }
